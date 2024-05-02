fcc3f499a00d6be96207e0997d1152157dd40bc9
jirgu <jirgu@SPEEDYSGX>
2024-05-01 20:45:21 -0600

    Agregar archivos de proyecto.

---
+++ MasterEnXamarin/MasterEnXamarin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterEnXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerMas_Clicked(object sender, EventArgs e)
+        {
+            string valor = lblTexto.Text;
+            //1.- Ocultar botón.
+            btnVerMas.IsVisible = false;
+
+            //2.- Mostrar texto.
+            lblTexto.LineBreakMode = LineBreakMode.WordWrap;
+            //Application.Current.MainPage.DisplayAlert("Oops...", "Algo salió mal", "Ok");
+            //DisplayAlert("Oops...", valor, "Ok");
+        }
+    }
+}
